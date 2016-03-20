68b65b0a18241638123cb03851f8bbba5b968431
Pablo Clemente <paclema@gmail.com>
2016-03-20 13:55:50 +0100

    Added interrupt example for encoder

---
+++ sound_controller/arduino_fw/Encoder/Encoder.ino
@@ -0,0 +1,43 @@
+
+#define encoder0PinA 2
+#define encoder0PinB 3
+
+volatile unsigned int encoder0Pos = 0;
+static boolean rotating=false;
+
+void setup() {
+  pinMode(encoder0PinA, INPUT); 
+  digitalWrite(encoder0PinA, HIGH);       
+  pinMode(encoder0PinB, INPUT); 
+  digitalWrite(encoder0PinB, HIGH); 
+
+  attachInterrupt(0, rotEncoder, CHANGE);  
+  Serial.begin (115200);
+   pinMode(13, OUTPUT); 
+}
+
+void rotEncoder(){
+  rotating=true; 
+  // If a signal change (noise or otherwise) is detected
+  // in the rotary encoder, the flag is set to true
+}
+
+void loop() {
+  while(rotating) {
+    delay(2);
+    // When signal changes we wait 2 milliseconds for it to 
+    // stabilise before reading (increase this value if there
+    // still bounce issues)
+    if (digitalRead(encoder0PinA) == digitalRead(encoder0PinB)) {  
+      encoder0Pos++;
+    } 
+    else {                                   
+      encoder0Pos--;
+    }
+    rotating=false; // Reset the flag back to false
+    Serial.println(encoder0Pos);
+  }
+
+  if(encoder0Pos%2 == 0) digitalWrite(13,HIGH);
+    else digitalWrite(13,LOW);
+}
