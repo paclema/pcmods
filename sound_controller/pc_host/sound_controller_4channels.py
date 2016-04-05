84f33a2c2c38ef529a07c5a07c8eade89b43fcf7
Pablo Clemente <paclema@gmail.com>
2016-04-05 03:56:29 +0200

    Added first commit for 4channels python host

---
+++ sound_controller/pc_host/sound_controller_4channels.py
@@ -0,0 +1,48 @@
+import time
+import warnings
+import serial
+import os
+
+arduino = serial.Serial('COM4', baudrate=115200, timeout=1.0)
+arduino.setDTR(False)
+time.sleep(1)
+arduino.flushInput()
+arduino.setDTR(True)
+
+#get volume from Channels
+
+channel_program = ["TeamSpeak 3 Client","Google Chrome","Origin","Telegram Desktop"]
+
+with arduino:
+    while True:
+        try:
+            text = arduino.readline()
+            if not text == "":
+            	#print(line)
+            	#os.system("SoundVolumeView.exe /Mute \"chrome.exe\"")
+                #text example recved by SPP: text = "ch_volume:25,36,45,56"
+                text = text.split(":")
+                if text[0] == "ch_volume":
+                    """"
+                    print("Sound: Cascos")
+                    os.system("SoundVolumeView.exe /SetDefault \"{0.0.0.00000000}.{0f5e5f19-525f-44d8-81b2-d978b5c28ccd}\" 1")
+                    os.system("SoundVolumeView.exe /SetDefault \"{0.0.0.00000000}.{0f5e5f19-525f-44d8-81b2-d978b5c28ccd}\" 2")
+                elif line == "B\r\n":
+                    print("Sound: Altavoces")
+                    os.system("SoundVolumeView.exe /SetDefault \"{0.0.0.00000000}.{fee286a0-8402-4859-99ca-9f8d785ffbd3}\" 1")
+                    os.system("SoundVolumeView.exe /SetDefault \"{0.0.0.00000000}.{fee286a0-8402-4859-99ca-9f8d785ffbd3}\" 2")
+                    #os.system("SHUTDOWN /S /T 10 ")
+                    """
+                    ch_volumes = list(text[1].split(","))
+
+                    for i in channel_program
+                        os.system("SoundVolumeView.exe /SetVolume " + channel_program[i] + " " + ch_volumes[i])
+
+
+                continue
+        except ValueError:
+            warnings.warn("text {} didn't parse, skipping".format(text))
+            print(text)
+        except KeyboardInterrupt:
+            print("Exiting")
+            break
