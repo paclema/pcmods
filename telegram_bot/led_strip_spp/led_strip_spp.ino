0428c58f9dd79911eaaf6b24513000b34d89d40d
Pablo Clemente <paclema@gmail.com>
2016-04-19 22:27:10 +0200

    Updated led_strip_spp.ino scketch to correct led_strip model and number of leds

--- telegram_bot/led_strip_spp/led_strip_spp.ino
+++ telegram_bot/led_strip_spp/led_strip_spp.ino
@@ -18,7 +18,7 @@ extern uint8_t I2C_SLA;
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(52, PIN, NEO_GRB + NEO_KHZ400);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(51, PIN, NEO_GRB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
@@ -90,9 +90,6 @@ void setup()
   strip.begin();
 
   strip.show(); // Initialize all pixels to 'off'
-
-  
-
 
   Serial.begin(19200);
 
