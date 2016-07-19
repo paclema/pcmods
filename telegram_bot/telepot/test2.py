d137bfc875e0e6caec6d3bd97bc8ccc8431f10d8
Pablo Clemente <paclema@gmail.com>
2016-07-19 14:43:45 +0200

    Updated test2.py to get info from the msg dict

--- telegram_bot/telepot/test2.py
+++ telegram_bot/telepot/test2.py
@@ -6,12 +6,18 @@ import telepot
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
-    print(content_type, chat_type, chat_id)
+
+    from_user =  msg.get('from')
+    username = from_user.get('username')
+    first_name = from_user.get('first_name')
+
+    print(content_type, chat_type, chat_id, username, first_name)
+
 
     if content_type == 'text':
         bot.sendMessage(chat_id, msg)
     if content_type == 'new_chat_member':
-        bot.sendMessage(chat_id, "wellcome to this awesome group!")
+        bot.sendMessage(chat_id, "Wellcome to this awesome group!")
     if content_type == 'left_chat_member':
         bot.sendMessage(chat_id, "Sayonara baby")
 
